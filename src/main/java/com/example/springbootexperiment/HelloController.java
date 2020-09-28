6ed716c3cd58550adc3c381f66d891b7142139c6
JennStro <jennys1994@gmail.com>
2020-09-28 23:31:55 +0200

    Added hello controller

---
+++ src/main/java/com/example/springbootexperiment/HelloController.java
@@ -0,0 +1,11 @@
+package com.example.springbootexperiment;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/")
+    public String index() {return "Hello from Spring boot!";}
+}
